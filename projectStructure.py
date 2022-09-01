ee025a7efa1c0d466db9e4714c4d092840a28de8
hes <h456ad@gmail.com>
2022-09-01 20:49:35 +0200

    first

--- projectStructure.py
+++ projectStructure.py
@@ -2,7 +2,8 @@ import os
 folders = ["asset","css","js"]
 
 projectName = input("Project Name: ")
-html_files ="""<!DOCTYPE html>
+def html_files(proname) :
+ return """<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -10,7 +11,7 @@ html_files ="""<!DOCTYPE html>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/index.css"/>
 
-    <title>"""+projectName+"""</title>
+    <title>"""+proname+"""</title>
 </head>
 <body>
     
@@ -18,21 +19,28 @@ html_files ="""<!DOCTYPE html>
 </body>
 </html>
 """
+def createFile(path):
+    return open(path,"w")
+def errormsg(msg):
+    print(msg)
 def CreateThePro(name):
-        try:
+    if os.path.exists(name):
+       errormsg("Project Name already exists: " + name)
+       Name = input("Project Name: ")
+       CreateThePro(Name)
+
+    else:
                 os.mkdir(name)
                 for folder in folders:
                     os.mkdir(name+"/"+folder)
-                open(name+"/js/index.js", "w")
-                open(name+"/css/index.css", "w")
-                with open(projectName+"/index.html", "w") as f:
-                   f.write(html_files)
+                createFile(name+"/js/index.js")
+                createFile(name+"/css/index.css")
+                with createFile(name+"/index.html") as f:
+                   f.write(html_files(name))
                 try:   
                    os.system("code ./"+name)
                 except:
-                    print("Error in vscode")
-        except:
-            print("Project Name already exists: " + name)
-
+                    errormsg("Error in vscode")
+ 
 
 CreateThePro(projectName)
